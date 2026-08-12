bf4c4b2789204354fe77b3a3cb67ec76c11298b2
noran <zzznoura95@gmail.com>
2026-08-12 22:53:54 +0300

    Complete Java 8 lambda and streams assignment

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("========== LAMBDA EXAMPLE ==========");
+        LambdaExample.main(args);
+
+        System.out.println("\n========== STREAMS EXAMPLE ==========");
+        StreamsExample.main(args);
+    }
+}
